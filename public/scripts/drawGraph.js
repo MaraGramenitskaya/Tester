852c63926327c0c973962a399f4a3c441fd6ed58
MaraGramenitskaya <mara.gr2000@gmail.com>
2024-08-14 11:46:03 +0300

    commit

---
+++ public/scripts/drawGraph.js
@@ -0,0 +1,174 @@
+const tester = document.getElementById("tester")
+const colors = ["rgb(193, 101, 243)", "rgb(152, 17, 117)", "rgb(20, 221, 145)", "rgb(250, 230, 50)", "rgb(23, 79, 109)", "rgb(105, 244, 5)", "rgb(96, 20, 238)", "rgb(237, 91, 91)", "rgb(57, 186, 255)", "rgb(20, 221, 145)", "rgb(250, 230, 50)", "rgb(23, 79, 109)", "rgb(105, 244, 5)", "rgb(96, 20, 238)", "rgb(237, 91, 91)", "rgb(57, 186, 255)"]
+
+const options = {
+    scales: {
+        x: {
+            borderWidth: 1,
+            pointStyle: "circle",
+            radius: 2,
+            hoverRadius: 3
+        }
+    },
+    plugins: {
+        legend: {
+            display: true,
+            labels: {
+                usePointStyle: true,
+                pointStyle: "rectRounded",
+                color: "rgb(102, 102, 102)",
+                font: {
+                    family: "sans-serif",
+                    size: 14
+                },
+                boxWidth: 15,
+                boxHeight: 15
+            }
+        }
+    }
+}
+
+const ctx1 = document.getElementById("myChart").getContext("2d");
+const ctx2 = document.getElementById("myChart2").getContext("2d");
+
+let chart1;
+let chart2;
+
+tester.addEventListener("click", () => {
+    window.location.href = "/";
+});
+
+function createChart1(ctx, data, options) {
+    if (chart1) {
+        chart1.destroy();
+    }
+    chart1 = new Chart(ctx, {
+        type: "line",
+        data: data,
+        options: options
+    })
+}
+function createChart2(ctx, data, options) {
+    if (chart2) {
+        chart2.destroy();
+    }
+    chart2 = new Chart(ctx, {
+        type: "line",
+        data: data,
+        options: options
+    })
+}
+
+function extractData(data) {
+    const timestamps = data.map(row => row.timestamp.split("T")[1].split(".")[0]);
+    const datasets1 = [];
+    const datasets2 = [];
+    let i = 0;
+    for (let key in data[0]) {
+        if (key !== "timestamp") {
+            const label = key.replace(/_/g, " ");
+            if (key.includes("Trigger")) {
+                const dataset = data.map(row => row[key]);
+                datasets2.push({
+                    label: label,
+                    data: dataset,
+                    backgroundColor: colors[i],
+                    borderColor: colors[i]
+                })
+                i++
+            } else {
+                const dataset = data.map(row => row[key]);
+                datasets1.push({
+                    label: label,
+                    data: dataset,
+                    backgroundColor: colors[i],
+                    borderColor: colors[i]
+                })
+                i++
+            }
+        }
+
+    }
+    return { timestamps, datasets1, datasets2 };
+}
+
+function createGraph(data) {
+    const { timestamps, datasets1, datasets2 } = extractData(data)
+    const chartData1 = {
+        labels: timestamps,
+        datasets: datasets1
+    }
+    const chartData2 = {
+        labels: timestamps,
+        datasets: datasets2
+    }
+    createChart1(ctx1, chartData1, options);
+    createChart2(ctx2, chartData2, options);
+}
+
+fetch("/getDataBySession")
+    .then(response => response.json())
+    .then(data => createGraph(data))
+    .catch(error => console.error("Error fetching data:", error));
+
+document.querySelector(".drop").addEventListener("click", function (event) {
+    this.querySelector("div").style.display = "flex";
+    event.stopPropagation();
+});
+
+window.addEventListener("click", function () {
+    document.querySelector(".drop div").style.display = "none";
+});
+
+document.getElementById("lastSession").addEventListener("click", async () => {
+    const response = await fetch("/getLastSession", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    const lastSession = await response.json();
+    document.getElementById("session").value = lastSession.lastSession;
+
+})
+
+document.getElementById("seeSession").addEventListener("click", function () {
+    const dateInput = document.getElementById("time1");
+    const date = dateInput.value;
+    if (date) {
+        fetch("/getSessions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ date: date })
+        })
+            .then(response => response.json())
+            .then(sessions => {
+                const sessionsList = document.getElementById("sessions-list");
+                sessionsList.innerHTML = "";
+                sessions.forEach(session => {
+                    const option = document.createElement("option");
+                    option.value = session;
+                    sessionsList.appendChild(option);
+                });
+            })
+            .catch(error => console.error("Ошибка:", error));
+    }
+});
+
+document.getElementById("confirm").addEventListener("click", function () {
+    const sessionValue = document.getElementById("session").value;
+    if (confirm(`Are you sure you want to see ${sessionValue} session?`)) {
+        fetch("/confirmSession", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ session: sessionValue })
+        })
+            .then(response => response.json())
+            .then(data => createGraph(data))
+            .catch(error => console.error("Error:", error));
+    }
+});
---
+++ public/scripts/mainPage.js
@@ -0,0 +1,113 @@
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const minInput = document.getElementById("min");
+const secInput = document.getElementById("sec");
+const graph = document.getElementById("seeGraph")
+let timerId = null;
+
+const timer = {
+    start: () => {
+        const min = Math.max(0, parseInt(minInput.value, 10) || 0);
+        const sec = Math.min(59, Math.max(0, parseInt(secInput.value, 10) || 0));
+        const checkboxStates = Array.from(document.querySelectorAll(".dvoynoy input[type='checkbox']")).map(checkbox => checkbox.checked);
+        if (min > 0 || sec > 0) {
+            if (confirm(`Are you sure you want to start ${min} min ${sec} sec session? `)) {
+                fetch("/start", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ min, sec, checkboxStates })
+                })
+                    .then(response => response.text())
+                    .then(data => console.log(data))
+                    .catch(error => console.error(error));
+
+                setTimeout(() => {
+                    console.log(`Session is over`);
+                }, min * 60 * 1000 + sec * 1000);
+            }
+        }
+    },
+    stop: () => {
+        fetch("/stop", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" }
+        })
+            .then(response => response.text())
+            .then(message => console.log(message))
+            .catch(error => console.error(error));
+    }
+}
+
+startBtn.addEventListener("click", timer.start);
+stopBtn.addEventListener("click", timer.stop);
+
+function sendMessage() {
+    const input = document.getElementById("myInput");
+    const inputValue = (input.value * 10).toString().substr(0, 3) / 10
+    if (confirm(`Are you sure you want to set Max Temp: "${inputValue}"?`)) {
+        const setTemp = document.getElementById("setTemp");
+        setTemp.innerText = inputValue;
+        fetch("/send-message", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ input: inputValue })
+        })
+            .then(response => response.text())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    }
+}
+
+function updateElements(message) {
+    if (message) {
+        const values = message.split(",");
+        const sample = document.getElementById("sample");
+        const maxTemp = document.getElementById("setTemp");
+        const tempElements = ["temp1", "temp2", "temp3", "temp4", "temp5", "temp6", "temp7"].map(id => document.getElementById(id));
+        const triggerElements = ["trigger1", "trigger2", "trigger3", "trigge4", "trigger5", "trigge6", "trigge7"].map(id => document.getElementById(id));
+
+        sample.innerText = values[0];
+        maxTemp.innerText = values[1];
+        for (let i = 0; i < 7; i++) {
+            const tempValue = values[2 + i * 2];
+            const triggerValue = values[3 + i * 2];
+            if (tempValue === "error") {
+                tempElements[i].innerText = "Error";
+                tempElements[i].style.backgroundColor = "#ff3c00";
+                triggerElements[i].style.backgroundColor = "#ff3c00";
+                triggerElements[i].innerText = "Error";
+            } else {
+                tempElements[i].innerText = tempValue;
+                tempElements[i].style.backgroundColor = "#FF9100";
+                triggerElements[i].style.backgroundColor = triggerValue === "1" ? "#9dff00" : "#FF9100";
+                triggerElements[i].innerText = `Trigger${i + 1}`;
+            }
+        }
+    }
+}
+
+setInterval(() => {
+    fetch("/checkUpdates")
+        .then(response => response.text())
+        .then(message => updateElements(message));
+}, 1000);
+
+graph.addEventListener("click", () => {
+    window.location.href = "/graph";
+});
+
+for (let i = 1; i <= 7; i++) {
+    const div = document.createElement("div");
+    div.className = "dvoynoy";
+    div.innerHTML = `
+        <p>${i}</p>
+        <div id="temp${i}" class="sensor temp display">Temp ${i}</div>
+        <div id="trigger${i}" class="sensor display">Trigger ${i}</div>
+        <input type="checkbox" id="checkbox${i}" checked>
+        `;
+    document.getElementById("big").appendChild(div);
+}
